9f12d21909ab092379f9a6dd9a18a439c0cd0483
Abdullah Alzayed <abdullah.z.alzayed@gmail.com>
2023-06-10 00:55:28 +0300

    initialized prime exercise

---
+++ solutions/numbers/prime_factors/target/debug/deps/prime_factors-42224b3d2df9facb.d
@@ -0,0 +1,5 @@
+/Users/majellan/Projects/Projects/solutions/numbers/prime_factors/target/debug/deps/prime_factors-42224b3d2df9facb.rmeta: src/main.rs
+
+/Users/majellan/Projects/Projects/solutions/numbers/prime_factors/target/debug/deps/prime_factors-42224b3d2df9facb.d: src/main.rs
+
+src/main.rs:
---
+++ solutions/numbers/prime_factors/target/debug/deps/prime_factors-a86abcf8512073c6.d
@@ -0,0 +1,5 @@
+/Users/majellan/Projects/Projects/solutions/numbers/prime_factors/target/debug/deps/prime_factors-a86abcf8512073c6.rmeta: src/main.rs
+
+/Users/majellan/Projects/Projects/solutions/numbers/prime_factors/target/debug/deps/prime_factors-a86abcf8512073c6.d: src/main.rs
+
+src/main.rs:
